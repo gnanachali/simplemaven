deb21e4ae5a0fc2d2fa17bab8e9b7b27183d508c
gnanasagar Chalicheemala <gnachali@publicisgroupe.net>
2022-04-27 18:01:28 +0530

    first commit

---
+++ mavensample/src/main/java/com/sapient/mavensample/App.java
@@ -0,0 +1,17 @@
+package com.sapient.mavensample;
+
+import com.sapient.endur.ui.Hello;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        new Hello();
+    }
+}
